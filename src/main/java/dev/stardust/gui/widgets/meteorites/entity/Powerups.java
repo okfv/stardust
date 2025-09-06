743f149405cfa4750105ceaed18c968c0dafc3fc
0xTas <admin@0xtas.dev>
2025-09-06 04:46:34 -0700

    Meteorites - Updated powerup description for accuracy.

--- src/main/java/dev/stardust/gui/widgets/meteorites/entity/Powerups.java
+++ src/main/java/dev/stardust/gui/widgets/meteorites/entity/Powerups.java
@@ -18,7 +18,7 @@ public enum Powerups implements StringIdentifiable {
             case RAPID_FIRE -> "Rapid Fire"; // fire rate doubled
             case BULLET_HELL -> "Bullet Hell"; // bullets naturally live longer
             case PHASE_SHIFT -> "Phase Shift"; // replaces right-click warp ability, briefly phase through meteorites
-            case MIDAS_TOUCH -> "Midas Touch"; // lol: shooting a bullet now costs 10 points, but is refunded with interest if it kills a meteorite
+            case MIDAS_TOUCH -> "Midas Touch"; // lol: shooting a bullet now costs points, but is refunded with interest if it kills a meteorite
             case GRAVITY_WELL -> "Gravity Well"; // only granted if mouse aim enabled, deploys at cursor to draw in nearby meteorites
             case HOMING_SHOTS -> "Homing Rounds"; // bullets track towards nearby meteorites
             case DOUBLE_POINTS -> "Double Points"; // kills award double points
